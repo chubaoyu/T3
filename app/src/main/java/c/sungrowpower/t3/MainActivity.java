89890a5e7e3ded2af9907d690ef92953df2ace39
chubaoyu <chubaoyu@sungrowpower.com>
2018-09-02 07:59:44 +0800

    "first"

--- app/src/main/java/c/sungrowpower/t3/MainActivity.java
+++ app/src/main/java/c/sungrowpower/t3/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //this is br_chubaoyu
+        //this is master
+        //test
     }
 }
